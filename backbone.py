d7c631e8d502ca2ab0a30cf5a7db717e732cf452
notchatgpt <160557384+notchatgptbiz@users.noreply.github.com>
2026-04-15 22:30:19 -0400

    Update backbone.py

--- backbone.py
+++ backbone.py
@@ -4,17 +4,22 @@ wrongletters = 0
 listofwords = ["tree", "car", "nickel", "hangman", "penny"]
 selectedword = random.choice(listofwords)
 length = len(selectedword)
-print("Your word has " + str(length) + " letters")
+wrongguesses = 0
+if length > 6:
+    wrongguesses = 8
+else:
+    wrongguesses = 6
+print("Your word has " + str(length) + " letters, and you have " + str(wrongguesses) + " guesses.")
 display = ("_ " * length)
 print("Begin guessing letters")
-while "_" in display and wrongletters < 6:
+while "_" in display and wrongletters < wrongguesses:
     userinput = input()
     if userinput in selectedword:
-        print("You guessed a letter correctly, and you still have " + str(6 - wrongletters) + " guesses left!")
+        print("You guessed a letter correctly, and you still have " + str(wrongguesses - wrongletters) + " guesses left!")
     elif userinput not in selectedword:
         wrongletters += 1
-        print("You've guessed a letter incorrectly, and you have " + str(6 - wrongletters) + " guesses left!")
+        print("You've guessed a letter incorrectly, and you have " + str(wrongguesses - wrongletters) + " guesses left!")
     print(display)
 
-if wrongletters >= 6:
+if wrongletters >= wrongguesses:
     print("You've ran out of guesses!")
