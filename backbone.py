025efb094020bda5e6cbc2fb4bf0d547d6187aad
notchatgpt <160557384+notchatgptbiz@users.noreply.github.com>
2026-04-15 21:47:46 -0400

    Update backbone.py

--- backbone.py
+++ backbone.py
@@ -4,11 +4,14 @@ listofwords = ["tree", "car", "nickel", "hangman", "penny"]
 selectedword = random.choice(listofwords)
 length = len(selectedword)
 print("Your word has " + str(length) + " letters")
-print("Begin guessing letters")
-userinput = input()
-if userinput == " " or "" or "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9" or "0" or "-" or "+" or "_" or "=" or "," or "." or "?" or "/" or "!" or "@" or "#" or "$" or "%" or "^" or "&" or "*" or "(" or ")" or ":" or ";" or "{" or "}" or "[" or "]" or "|" or "<" or ">" or "`" or "~":
-    print("You've guessed: \"" + userinput + "\", which is an invalid response. Remember to only guess letters")
-elif userinput in selectedword:
-  print("You guessed a letter correctly!")
-else:
-  print("You've guessed a letter incorrectly!")
+display = ("_" * length)
+while "_" in display:
+    print("Begin guessing letters")
+    userinput = input()
+    if userinput == " " or "" or "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9" or "0" or "-" or "+" or "_" or "=" or "," or "." or "?" or "/" or "!" or "@" or "#" or "$" or "%" or "^" or "&" or "*" or "(" or ")" or ":" or ";" or "{" or "}" or "[" or "]" or "|" or "<" or ">" or "`" or "~":
+        print("You've guessed: \"" + userinput + "\", which is an invalid response. Remember to only guess letters")
+    elif userinput in selectedword:
+        print("You guessed a letter correctly!")
+    else:
+        print("You've guessed a letter incorrectly!")
+    print(display)
